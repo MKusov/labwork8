f7cd0efcb5eee8b6a54b1869efe929016b26fe32
Trenkill <michaelfarma84@gmail.com>
2016-11-13 23:13:46 +0300

    Update my_math_controller.rb

--- app/controllers/my_math_controller.rb
+++ app/controllers/my_math_controller.rb
@@ -5,8 +5,10 @@ class MyMathController < ApplicationController
 
   def view
   n=params[:n].to_i
-  @arr=[]
-  (1..n).each { |x| @arr << x if mersen(x) }
+    @arr=(1..1.0/0.0).
+      lazy.map{|x| 2**x - 1}
+           .take_while{|x| x < n}
+           .select{|x| Prime.prime?(x)}.to_a
   end
 
 end
